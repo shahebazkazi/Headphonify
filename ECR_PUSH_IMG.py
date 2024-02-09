fff6058e73789870fa624cca7b4fdb189a241df1
shahebazkazi <119780771+shahebazkazi@users.noreply.github.com>
2024-02-09 11:59:18 +0530

    Create ECR_PUSH_IMG.py

---
+++ ECR_PUSH_IMG.py
@@ -0,0 +1,36 @@
+import boto3
+def list_ecr_images(repository_name):
+    client = boto3.client('ecr')
+    response = client.describe_images(repositoryName=repository_name)
+    image_details = response['imageDetails']
+    for image in image_details:
+        print(f"Image Digest: {image['imageDigest']}, Image Tags: {image['imageTags'] if 'imageTags' in image else 'None'}")
+if __name__ == "__main__":
+    repository_name = "hedphony"  # Replace with your repository name
+    list_ecr_images(repository_name)
+
+React
+
+Reply
+
+
+
+
+
+
+
+
+
+
+
+
+Message Ammar Shaikh
+
+
+
+
+
+
+
+
+
