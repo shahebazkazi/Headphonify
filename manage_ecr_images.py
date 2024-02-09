da50336bd1ef3eb254ecb2fedfb13a63765bf2cb
shahebazkazi <119780771+shahebazkazi@users.noreply.github.com>
2024-02-09 18:03:15 +0530

    Update manage_ecr_images.py

--- manage_ecr_images.py
+++ manage_ecr_images.py
@@ -1,12 +1,15 @@
 import boto3
+
 def list_ecr_images(repository_name):
     client = boto3.client('ecr')
     response = client.describe_images(repositoryName=repository_name)
     return response['imageDetails']
+
 def print_ecr_images(repository_name):
     images = list_ecr_images(repository_name)
     for image in images:
-        print(f"Image Digest: {image['imageDigest']}, Image Tags: {image['imageTags'] if 'imageTags' in image else 'None'}")
+        print(f"Image Digest: {image['imageDigest']}, Image Tags: {image.get('imageTags', 'None')}")
+
 def delete_old_images(repository_name, num_to_keep):
     images = list_ecr_images(repository_name)
     images.sort(key=lambda x: x['imagePushedAt'], reverse=True)
@@ -15,18 +18,25 @@ def delete_old_images(repository_name, num_to_keep):
         client = boto3.client('ecr')
         for image in images_to_delete:
             image_digest = image['imageDigest']
-            client.batch_delete_image(
-                repositoryName=repository_name,
-                imageIds=[
-                    {
-                        'imageDigest': image_digest
-                    }
-                ]
-            )
-            print(f"Deleted image with digest: {image_digest}")
+            print(f"Deleting image with digest: {image_digest}")
+            # Prompt for confirmation before deletion
+            confirmation = input("Are you sure you want to delete this image? (yes/no): ")
+            if confirmation.lower() == 'yes':
+                client.batch_delete_image(
+                    repositoryName=repository_name,
+                    imageIds=[
+                        {
+                            'imageDigest': image_digest
+                        }
+                    ]
+                )
+                print(f"Deleted image with digest: {image_digest}")
+            else:
+                print("Image deletion canceled.")
     else:
         print("No images to delete.")
-if __name__ == "__main__":
+
+if _name_ == "_main_":
     repository_name = "hedphony"  # Your repository name
     num_to_keep = 10  # Number of image versions to keep
     # First, list all ECR images
