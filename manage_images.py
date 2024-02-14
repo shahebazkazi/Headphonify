d6d7aa71bb5ece80177869b094807e4fa63a777f
Ammar shaikh <152147660+ammar266274432@users.noreply.github.com>
2024-02-14 18:41:00 +0530

    Update manage_images.py
    new changes

--- manage_images.py
+++ manage_images.py
@@ -1,12 +1,21 @@
 import boto3
+
+def list_ecr_repositories():
+    client = boto3.client('ecr')
+    response = client.describe_repositories()
+    repositories = response['repositories']
+    return repositories
+
 def list_ecr_images(repository_name):
     client = boto3.client('ecr')
     response = client.describe_images(repositoryName=repository_name)
     return response['imageDetails']
+
 def print_ecr_images(repository_name):
     images = list_ecr_images(repository_name)
     for image in images:
         print(f"Image Digest: {image['imageDigest']}, Image Tags: {image['imageTags'] if 'imageTags' in image else 'None'}")
+
 def delete_old_images(repository_name, num_to_keep):
     images = list_ecr_images(repository_name)
     images.sort(key=lambda x: x['imagePushedAt'], reverse=True)
@@ -26,12 +35,22 @@ def delete_old_images(repository_name, num_to_keep):
             print(f"Deleted image with digest: {image_digest}")
     else:
         print("No images to delete.")
+
 if __name__ == "__main__":
+    # List all ECR repositories
+    print("Listing all ECR repositories:")
+    repositories = list_ecr_repositories()
+    for repo in repositories:
+        print(repo['repositoryName'])
+
+    # Specify the repository name and the number of image versions to keep
     repository_name = "shahebaz"  # Your repository name
     num_to_keep = 2  # Number of image versions to keep
-    # First, list all ECR images
-    print("Listing all ECR images:")
+
+    # First, list all ECR images for the specified repository
+    print("\nListing all ECR images for repository:", repository_name)
     print_ecr_images(repository_name)
+
     # Then, delete old images to keep only the specified number of latest images
     print("\nDeleting old images to keep only the latest", num_to_keep, "images:")
     delete_old_images(repository_name, num_to_keep)
