7a25daa8010195a8c3c5565b9d50edafe1b99a0e
shahebazkazi <119780771+shahebazkazi@users.noreply.github.com>
2024-02-14 17:34:05 +0530

    Delete #!/usr/bin directory

--- #!/usr/bin/cleanup_docker_images.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-# Define the main Node.js image name
-MAIN_NODEJS_IMAGE="node:16 "
-
-# Remove all Docker images except the main Node.js image
-docker images -q | grep -v $MAIN_NODEJS_IMAGE | xargs -I {} docker rmi -f {}
