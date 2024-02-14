68ee72d4cfc200d8de405c9141fc53774a27cb53
shahebazkazi <119780771+shahebazkazi@users.noreply.github.com>
2024-02-14 19:01:22 +0530

    Update manage_images.py

--- manage_images.py
+++ manage_images.py
@@ -1,15 +1,17 @@
 import boto3
-
+def list_ecr_repositories():
+    client = boto3.client('ecr')
+    response = client.describe_repositories()
+    repositories = response['repositories']
+    return repositories
 def list_ecr_images(repository_name):
     client = boto3.client('ecr')
     response = client.describe_images(repositoryName=repository_name)
     return response['imageDetails']
-
 def print_ecr_images(repository_name):
     images = list_ecr_images(repository_name)
     for image in images:
         print(f"Image Digest: {image['imageDigest']}, Image Tags: {image['imageTags'] if 'imageTags' in image else 'None'}")
-
 def delete_old_images(repository_name, num_to_keep):
     images = list_ecr_images(repository_name)
     images.sort(key=lambda x: x['imagePushedAt'], reverse=True)
@@ -29,14 +31,29 @@ def delete_old_images(repository_name, num_to_keep):
             print(f"Deleted image with digest: {image_digest}")
     else:
         print("No images to delete.")
-
 if __name__ == "__main__":
     repository_names = ["shahebaz", "ecrepo1", "ecrepo2"]  # List of repository names
-    num_to_keep = 2  # Number of image versions to keep
     for repository_name in repository_names:
-        # First, list all ECR images for each repository
-        print(f"Listing all ECR images for repository {repository_name}:")
-        print_ecr_images(repository_name)
+        # List all ECR repositories
+        print("Listing all ECR repositories:")
+        repositories = list_ecr_repositories()
+        for repo in repositories:
+            print(repo['repositoryName'])
+            # List all ECR images for each repository
+            print("\nListing all ECR images for repository:", repo['repositoryName'])
+            print_ecr_images(repo['repositoryName'])
+            print()  # Add a newline to separate outputs for each repository
+        # Specify the repository name and the number of image versions to keep
+        num_to_keep = 2  # Number of image versions to keep
         # Then, delete old images to keep only the specified number of latest images
-        print(f"\nDeleting old images for repository {repository_name} to keep only the latest {num_to_keep} images:")
+        print("\nDeleting old images to keep only the latest", num_to_keep, "images:")
         delete_old_images(repository_name, num_to_keep)
+
+
+
+
+
+
+
+
+
